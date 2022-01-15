509c08f303b1197f0791332115ca911f14990725
kevin123488 <kevin123488@naver.com>
2022-01-15 15:19:02 +0900

    int, str 적용

--- python/pre_learning.py
+++ python/pre_learning.py
@@ -9,3 +9,13 @@ print('{0} + {1} = {2}'.format(a, b, a + b))
 name = input('당신의 이름을 입력하세요: ')
 age = input('당신의 나이를 입력하세요: ')
 print(f'이름: {name}, 나이: {age}')
+
+c = 3
+d = '문자'
+print(str(c) + d)
+#str()을 이용해 숫자를 문자열로 바꿔줄 수 있음
+
+e = 5
+f = '3'
+print(e + int(f))
+#int()를 이용해 문자열을 숫자로 바꿔줄 수 있음
