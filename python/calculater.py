55085ed2de0a5dd01e85923703b950e7bc84edf8
kevin123488 <kevin123488@naver.com>
2022-01-17 22:21:40 +0900

    1/17 study

--- python/calculater.py
+++ python/calculater.py
@@ -70,11 +70,56 @@
 # if count == 2:
 #     print(f'{a}(은)는 1과 {a}로만 나눌 수 있습니다.')
 
-a = [88, 30, 61, 55, 95]
-count = 1
-for i in a:
-    if i >= 60:
-        print(f'{count}번 학생의 점수는 {i}점으로, 합격입니다.')
-    else:
-        print(f'{count}번 학생의 점수는 {i}점으로, 불합격입니다.')
-    count += 1
+# a = [88, 30, 61, 55, 95]
+# count = 1
+# for i in a:
+#     if i >= 60:
+#         print(f'{count}번 학생의 점수는 {i}점으로, 합격입니다.')
+#     else:
+#         print(f'{count}번 학생의 점수는 {i}점으로, 불합격입니다.')
+#     count += 1
+
+# str(1) # (1)
+# int('30') # (2)
+# int(5) # (3)
+# bool('50') # (4)
+# int('35') # (5)
+
+# n = 5
+# m = 9
+# ga_ro = n * '*'
+# for i in range(m):
+# 	print(ga_ro)
+
+# n = 5
+# m = 9
+# ga_ro = '*' * n + '\n'
+# sagak_h = ga_ro * m
+# print(sagak_h)
+
+# print('''\"파일은 c:\\Windows\\Users\\내문서\\Python에 저장이 되었습니다.\"\n나는 생각했다. \'cd를 써서 git bash로 들어가 봐야지.\'''')
+
+#근의공식
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# ans1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+# ans2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+
+# print(ans1, ans2)
+
+# a = int(input())
+# for i in range(1, a + 1):
+# 	print(i)
+
+# a = int(input())
+# for i in range(a, -1, -1):
+# 	print(i)
+
+a = int(input())
+ans = 0
+for i in range(a + 1):
+	ans = ans + i
+print(ans)
